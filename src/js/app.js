3cda418fe9fa59bf461194204f1aeed5b3c1aa89
Viktor <mr.viktor.tkachev@gmail.com>
2023-03-11 17:23:17 +0800

    change the sort of props in for-in method

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,19 @@
 function orderByProps(obj, sortProps) {
   const result = [];
   let remainingProps = [];
+  const newSortProp = [];
 
   for (const prop in obj) {
     if (Object.prototype.hasOwnProperty.call(obj, prop)) {
       if (sortProps.includes(prop) === false) {
         remainingProps.push(prop);
+      } else {
+        newSortProp.push(prop);
       }
     }
   }
 
-  remainingProps = [...sortProps, ...remainingProps.sort()];
+  remainingProps = [...newSortProp, ...remainingProps.sort()];
 
   for (const prop of remainingProps) {
     result.push({ key: prop, value: obj[prop] });
